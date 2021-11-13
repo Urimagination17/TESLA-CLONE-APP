a7776ddd815744baa9f073d29d8cd9e6b3eb4128
Rohan <rohanb17000@gmail.com>
2021-11-13 15:48:04 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
+import Section from './components/Section';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <Section />
+    </div>
+  );
+}
+
+export default App;
